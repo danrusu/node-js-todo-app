123146c8a5c297118051c4375ea739fd18daa301
Dan Rusu <dangingko@yahoo.com>
2024-11-11 21:38:22 +0200

    improvements

--- src/html/main.js
+++ src/html/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:1112/api/todo';
+const API_BASE_URL = 'http://localhost:1112/api';
 
 const prettify = json => JSON.stringify(json, null, 2);
 const logJson = json => console.log(prettify(json));
@@ -11,66 +11,13 @@ const setText = (id, text) => (selectById(id).innerHTML = text);
 selectById('todoId').value = 0;
 
 const todoApiFetch = async (urlPath, options) => {
-  const response = await fetch(`${API_URL}${urlPath || ''}`, options);
+  const response = await fetch(`${API_BASE_URL}/todo${urlPath || ''}`, options);
   return await response.json();
 };
 
-const createTodoHtml = todoJson => {
-  const todoClass = `priority-${todoJson.priority}-todo`;
-
-  const div = (content, className) => {
-    const divClass = className ? ` class="${className}"` : '';
-    return `<div${divClass}>${content}</div>`;
-  };
-
-  const { name, description, id, date, time } = todoJson;
-  const dateTimeDiv = (date, time) => {
-    if (!date || (!date && !time)) {
-      return div('');
-    }
-    if (!time) {
-      return div(date);
-    }
-    return div(`${date}&nbsp; &nbsp;${time}`);
-  };
-  const todoInfoDiv =
-    div(name, 'name') +
-    div(description) +
-    div(`ID: ${id}`) +
-    dateTimeDiv(date, time);
-
-  return div(todoInfoDiv, todoClass);
-};
-
-const isTodoValid = () => {
-  if (selectById('todoName').value === '') {
-    alert('Name is mandatory.');
-    return false;
-  }
-  return true;
-};
-
-const getTodoJson = () => {
-  const todoJson = selectAll('#form input, #form select').reduce(
-    (acc, input) => {
-      // id = todoName => key = name
-      const key = input.id.replace('todo', '').toLowerCase();
-      acc[key] = ['id', 'priority'].includes(key)
-        ? parseInt(input.value)
-        : input.value;
-
-      return acc;
-    },
-    {},
-  );
-  return JSON.stringify(todoJson);
-};
-
-const resetTodos = () => (selectById('todos').innerHTML = '');
-
 const actions = {
   getAllTodos: async () => {
-    const responseJson = await todoApiFetch();
+    const responseJson = await todoApiFetch('');
     resetTodos();
 
     responseJson
@@ -126,6 +73,88 @@ const actions = {
   },
 };
 
+const todoInfoAlert = async id => {
+  const { name, priority, description, date, time } = await actions.getTodo(id);
+  const info = {
+    TODO: name,
+    Priority: priority,
+    Description: description,
+    Date: date,
+    Time: time,
+  };
+  alert(
+    Object.entries(info)
+      .filter(([_, v]) => v !== '')
+      .map(([k, v]) => `${k}: ${v}`)
+      .join('\n'),
+  );
+};
+
+// todo component
+const tag = tagName => (content, tagAttributes) => {
+  const attributes = Object.entries(tagAttributes || {})
+    .map(([k, v]) => `${k}="${v}"`)
+    .join(' ');
+  return `<${tagName} ${attributes}>${content}</${tagName}>`;
+};
+
+const [div, span] = ['div', 'span'].map(tag);
+
+const createTodoHtml = todoJson => {
+  const todoClass = `priority-${todoJson.priority}-todo`;
+  const { name, description, id, date, time } = todoJson;
+  const dateTimeDiv = (date, time) => {
+    if (!date || (!date && !time)) {
+      return div('');
+    }
+    if (!time) {
+      return div(date, { onclick: `todoInfoAlert(${id})` });
+    }
+    return div(`${date}&nbsp; &nbsp;${time}`, {
+      onclick: `todoInfoAlert(${id})`,
+    });
+  };
+  const todoInfoDiv =
+    span('x', {
+      class: 'delete',
+      onclick: `actions.deleteTodo(${id})`,
+    }) +
+    div(name, { class: 'name', onclick: `todoInfoAlert(${id})` }) +
+    div(description, { onclick: `todoInfoAlert(${id})` }) +
+    div(`ID: ${id}`, { onclick: `todoInfoAlert(${id})` }) +
+    dateTimeDiv(date, time);
+
+  return div(todoInfoDiv, {
+    class: todoClass,
+  });
+};
+
+const isTodoValid = () => {
+  if (selectById('todoName').value.length < 5) {
+    alert('Name should have at least 5 characters.');
+    return false;
+  }
+  return true;
+};
+
+const getTodoJson = () => {
+  const todoJson = selectAll('#form input, #form select').reduce(
+    (acc, input) => {
+      // id = todoName => key = name
+      const key = input.id.replace('todo', '').toLowerCase();
+      acc[key] = ['id', 'priority'].includes(key)
+        ? parseInt(input.value)
+        : input.value;
+
+      return acc;
+    },
+    {},
+  );
+  return JSON.stringify(todoJson);
+};
+
+const resetTodos = () => (selectById('todos').innerHTML = '');
+
 const addTodoActionToButton = buttonElement => {
   console.log(todo[button.id]);
   buttonElement.addEventListener('click', todo[button.id]);
@@ -135,11 +164,8 @@ const getTodoJsonBody = () => JSON.parse(selectById('todoBody').value);
 const getTodoId = () => selectById('todoId').value;
 
 const buttonsClickEventHandlers = {
-  getAllTodos: actions.getAllTodos,
-  getTodo: () => actions.getTodo(getTodoId()),
   createTodo: () => actions.createTodo(getTodoJson()),
   updateTodo: () => actions.updateTodo(getTodoId(), getTodoJson()),
-  deleteTodo: () => actions.deleteTodo(getTodoId()),
   deleteAllTodos: actions.deleteAllTodos,
 };
 
