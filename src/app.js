8347c86aabdbf23eeb5c17d6f0a9fbbe088adc07
Dan Rusu <dangingko@yahoo.com>
2024-12-08 20:33:22 +0200

    basic auth, serve html folder as public / static

--- src/app.js
+++ src/app.js
@@ -2,31 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.json());
-app.use(express.static('public'));
-
-const serveFileFromRoot = (res, relativePath) =>
-  res.sendFile(path.join(__dirname, relativePath));
-
-// static files
-const [serveHome, serveCss, serveMainJs, serveTodoClientJs, serveFavicon] = [
-  'index.html',
-  'main.css',
-  'main.js',
-  'todo-http-client.js',
-  'favicon.ico',
-].map(filePath => (_, res) => serveFileFromRoot(res, `html/${filePath}`));
-
-// routes
-app.get('/api/health-check', (_, res) => {
-  res.send({ status: 'healthy' });
-});
-app.get('/', serveHome);
-app.get('/css', serveCss);
-app.get('/main', serveMainJs);
-app.get('/todo-http-client', serveTodoClientJs);
-app.get('/favicon', serveFavicon);
-
 const {
   getAll,
   getTodo,
@@ -35,10 +10,79 @@ const {
   updateTodo,
 } = require('./dataController');
 
+// middlewares
+app.use(express.json());
+app.use(express.static('src/html'));
+app.use(basicAuthenticationMidleware);
+
+// routes
+app.get('/', serveHome);
+
+app.get('/api/health-check', isHealthy);
 app.get('/api/todo', getAll);
 app.get('/api/todo/:id', getTodo);
 app.post('/api/todo', createTodo);
 app.put('/api/todo/:id', updateTodo);
 app.delete('/api/todo', deleteTodo);
+
+// ********** functions
+function serveHome(_, res) {
+  serveFileFromRoot(res, 'index.html');
+}
+
+function serveFileFromRoot(res, relativePath) {
+  res.sendFile(path.join(__dirname, relativePath));
+}
+
+function isHealthy(_, res) {
+  res.send({ status: 'healthy' });
+}
+
+function basicAuthenticationMidleware(req, res, next) {
+  const auth = { username: 'tester', password: '12345' }; // this should not be in the code :)
+
+  const validCredentials = (username, password) =>
+    username === auth.username && password === auth.password;
+
+  const { username, password } = parseBasicAuthenticationHeader(req);
+
+  if (!username || !password) {
+    denyAccess(res);
+    return;
+  }
+
+  // Verify login and password are set and correct
+  if (validCredentials(username, password)) {
+    // access granted
+    return next();
+  }
+
+  denyAccess(res);
+}
+
+function denyAccess(res) {
+  res.set('WWW-Authenticate', 'Basic realm="401"');
+  res.status(401).send('Authentication required.');
+}
+
+function parseBasicAuthenticationHeader(req) {
+  // Authorization: 'Basic base64('username:password')
+  const [authorizationType, base64Authorization] = (
+    req.headers.authorization || ''
+  ).split(' ');
+
+  if (authorizationType !== 'Basic') {
+    return {};
+  }
+
+  const decodedAuthorization = Buffer.from(
+    base64Authorization,
+    'base64',
+  ).toString();
+
+  const [username, password] = decodedAuthorization.split(':');
+
+  return { username, password };
+}
 
 module.exports = { app };
