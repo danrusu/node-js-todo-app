1cc468d8f0f5247bdc4f7f5258bac46d17c0efc3
Dan Rusu <dangingko@yahoo.com>
2025-02-18 12:20:18 +0200

    fixes

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -2,13 +2,14 @@
 
 describe('smoke test', () => {
   it('redirects to login page if not authenticated', () => {
-    // let pageTitle;
+    let pageTitle;
 
     cy.visit('http://localhost:1112/');
 
     cy.url().then(url => {
       cy.logToTerminal(url);
     });
+    cy.task('logToTerminal', `@@@ abc`);
 
     cy.location().then(location => {
       cy.logToTerminal(JSON.stringify(location));
@@ -21,15 +22,15 @@ describe('smoke test', () => {
 
     // then: not retried
     cy.title().then(title => {
-      // pageTitle = title;
-      // console.log('[1] pageTitle', pageTitle);
+      pageTitle = title;
+      console.log('[1] pageTitle', pageTitle);
       cy.expect(title).equals('login');
     });
 
-    // console.log('[2] spageTitle', pageTitle);
+    console.log('[2] spageTitle', pageTitle);
 
-    // cy.wrap(null).then(() => {
-    //   console.log('[3] spageTitle', pageTitle);
-    // });
+    cy.wrap(null).then(() => {
+      console.log('[3] spageTitle', pageTitle);
+    });
   });
 });
